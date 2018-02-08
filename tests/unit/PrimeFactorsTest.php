088cd122aa996fb9448215aa08620a593c92fb43
Willem Leuverink <willem@leuver.ink>
2018-02-08 13:02:33 +0100

    added & configured phpunit

---
+++ tests/unit/PrimeFactorsTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class PrimeFactorsTest extends TestCase
+{
+    /** @test */
+    public function can_generate_prime_factors()
+    {
+        // TODO: Test drive out the new feature...
+    }
+}
